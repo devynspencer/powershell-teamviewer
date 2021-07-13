8d7002866d3687c3f3d7a52ec60290ff90ffbb64
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-07-12 17:11:25 -0700

    Add parameter set for listing devices by group id

--- Public/Get-TeamViewerDevice.ps1
+++ Public/Get-TeamViewerDevice.ps1
@@ -1,6 +1,11 @@
 function Get-TeamViewerDevice {
+    [CmdletBinding(DefaultParameterSetName = 'ByDeviceId')]
     param (
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = 'ByDeviceId')]
         $Id,
+
+        [Parameter(ValueFromPipelineByPropertyName, ParameterSetName = 'ByGroupId')]
+        $GroupId,
 
         [Parameter(Mandatory)]
         [ValidateNotNull()]
@@ -12,42 +17,68 @@ function Get-TeamViewerDevice {
     }
 
     process {
-        foreach ($DeviceId in $Id) {
-            $RestParams = @{
-                Headers = $Headers
-                Uri = "https://webapi.teamviewer.com/api/v1/devices/$DeviceId"
-            }
+        switch ($PSCmdlet.ParameterSetName) {
+            'ByDeviceId' {
+                foreach ($DeviceId in $Id) {
+                    $RestParams = @{
+                        Headers = $Headers
+                        Uri = "https://webapi.teamviewer.com/api/v1/devices/$DeviceId"
+                    }
 
-            $Response = Invoke-RestMethod @RestParams
+                    $Response = Invoke-RestMethod @RestParams
 
-            foreach ($Device in $Response.devices) {
-                [pscustomobject] @{
-                    Id = $Device.teamviewer_id
-                    RemoteControlId = $Device.remotecontrol_id
-                    DeviceId = $Device.device_id
-                    Alias = $Device.alias
-                    GroupId = $Device.groupid
-                    Status = $Device.online_state
+                    foreach ($Device in $Response.devices) {
+                        [pscustomobject] @{
+                            Id = $Device.teamviewer_id
+                            RemoteControlId = $Device.remotecontrol_id
+                            DeviceId = $Device.device_id
+                            Alias = $Device.alias
+                            GroupId = $Device.groupid
+                            Status = $Device.online_state
+                        }
+                    }
+                }
+
+                if (!$PSBoundParameters.ContainsKey('Id')) {
+                    $RestParams = @{
+                        Headers = $Headers
+                        Uri = "https://webapi.teamviewer.com/api/v1/devices"
+                    }
+
+                    $Response = Invoke-RestMethod @RestParams
+
+                    foreach ($Device in $Response.devices) {
+                        [pscustomobject] @{
+                            Id = $Device.teamviewer_id
+                            RemoteControlId = $Device.remotecontrol_id
+                            DeviceId = $Device.device_id
+                            Alias = $Device.alias
+                            GroupId = $Device.groupid
+                            Status = $Device.online_state
+                        }
+                    }
                 }
             }
-        }
 
-        if (!$PSBoundParameters.ContainsKey('Id')) {
-            $RestParams = @{
-                Headers = $Headers
-                Uri = "https://webapi.teamviewer.com/api/v1/devices"
-            }
+            'ByGroupId' {
+                foreach ($Group in $GroupId) {
+                    $RestParams = @{
+                        Headers = $Headers
+                        Uri = "https://webapi.teamviewer.com/api/v1/devices?groupid=$Group"
+                    }
 
-            $Response = Invoke-RestMethod @RestParams
+                    $Response = Invoke-RestMethod @RestParams
 
-            foreach ($Device in $Response.devices) {
-                [pscustomobject] @{
-                    Id = $Device.teamviewer_id
-                    RemoteControlId = $Device.remotecontrol_id
-                    DeviceId = $Device.device_id
-                    Alias = $Device.alias
-                    GroupId = $Device.groupid
-                    Status = $Device.online_state
+                    foreach ($Device in $Response.devices) {
+                        [pscustomobject] @{
+                            Id = $Device.teamviewer_id
+                            RemoteControlId = $Device.remotecontrol_id
+                            DeviceId = $Device.device_id
+                            Alias = $Device.alias
+                            GroupId = $Device.groupid
+                            Status = $Device.online_state
+                        }
+                    }
                 }
             }
         }
