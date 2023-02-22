b43dfd73d52b2e79cb07a72ad0c3c3c9801eb071
SPENCER Devyn * OYA <Devyn.Spencer@oya.oregon.gov>
2023-02-21 22:13:32 -0800

    Refactor Get-TeamViewerDevice
    Use generic Get-TeamViewerApiData function to standardize requests to each endpoint.

--- TeamViewer/Public/Get-TeamViewerDevice.ps1
+++ TeamViewer/Public/Get-TeamViewerDevice.ps1
@@ -1,86 +1,9 @@
+. "$PSScriptRoot\..\Private\Get-TeamViewerApiData.ps1"
+
 function Get-TeamViewerDevice {
-    [CmdletBinding(DefaultParameterSetName = 'ByDeviceId')]
-    param (
-        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = 'ByDeviceId')]
-        $Id,
+    $Response = Get-TeamViewerApiData -ApiEndpoint 'devices'
 
-        [Parameter(ValueFromPipelineByPropertyName, ParameterSetName = 'ByGroupId')]
-        $GroupId,
+    $Response.devices
 
-        [Parameter(Mandatory)]
-        [ValidateNotNull()]
-        $Token
-    )
-
-    begin {
-        $Headers = @{ authorization = "Bearer $Token" }
-    }
-
-    process {
-        switch ($PSCmdlet.ParameterSetName) {
-            'ByDeviceId' {
-                foreach ($DeviceId in $Id) {
-                    $RestParams = @{
-                        Headers = $Headers
-                        Uri = "https://webapi.teamviewer.com/api/v1/devices/$DeviceId"
-                    }
-
-                    $Response = Invoke-RestMethod @RestParams
-
-                    foreach ($Device in $Response.devices) {
-                        [pscustomobject] @{
-                            Id = $Device.teamviewer_id
-                            RemoteControlId = $Device.remotecontrol_id
-                            DeviceId = $Device.device_id
-                            Alias = $Device.alias
-                            GroupId = $Device.groupid
-                            Status = $Device.online_state
-                        }
-                    }
-                }
-
-                if (!$PSBoundParameters.ContainsKey('Id')) {
-                    $RestParams = @{
-                        Headers = $Headers
-                        Uri = "https://webapi.teamviewer.com/api/v1/devices"
-                    }
-
-                    $Response = Invoke-RestMethod @RestParams
-
-                    foreach ($Device in $Response.devices) {
-                        [pscustomobject] @{
-                            Id = $Device.teamviewer_id
-                            RemoteControlId = $Device.remotecontrol_id
-                            DeviceId = $Device.device_id
-                            Alias = $Device.alias
-                            GroupId = $Device.groupid
-                            Status = $Device.online_state
-                        }
-                    }
-                }
-            }
-
-            'ByGroupId' {
-                foreach ($Group in $GroupId) {
-                    $RestParams = @{
-                        Headers = $Headers
-                        Uri = "https://webapi.teamviewer.com/api/v1/devices?groupid=$Group"
-                    }
-
-                    $Response = Invoke-RestMethod @RestParams
-
-                    foreach ($Device in $Response.devices) {
-                        [pscustomobject] @{
-                            Id = $Device.teamviewer_id
-                            RemoteControlId = $Device.remotecontrol_id
-                            DeviceId = $Device.device_id
-                            Alias = $Device.alias
-                            GroupId = $Device.groupid
-                            Status = $Device.online_state
-                        }
-                    }
-                }
-            }
-        }
-    }
+    # TODO: Cleanup output object
 }
